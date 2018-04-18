a84df1b7713ae4e3f32b7bafc20b669f1b9e111a
Megan Penn <penn.meg@gmail.com>
2018-04-18 16:00:16 -0400

    shows no videos passing

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,10 @@
 const {assert} = require('chai');
+
+describe('user visits landing page', () => {
+  describe('no existing videos', () => {
+    it('shows no videos', () => {
+      browser.url('/');
+      assert.equal(browser.getText('#videos-container'), '');
+    });
+  });
+});
